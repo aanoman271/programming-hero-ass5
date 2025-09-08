0d27532f811d216af7e54e5c9530f0f8fb3b33b0
noman291 <aanoman291@gmail.com>
2025-09-09 02:31:52 +0600

    call button functionality done

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 document.getElementById('cards').addEventListener('click',function(e){
-    const dd=e.target
- if(dd.className.includes('call-btn')){
+    const callBtn=e.target
+ if(callBtn.className.includes('call-btn')){
+ const serviceName=callBtn.parentNode.parentNode.children[1].children[0].innerText;
+ const serviceNumber=callBtn.parentNode.parentNode.children[2].children[0].innerText;
+ const coinNum=document.getElementById('coin-num').innerText;
+
+ 
+const perClickCoin=Number(coinNum)-Number(20);
+
+if(perClickCoin<=0){
+   document.getElementById('coin-num').innerText='0';    
+    alert('husadiuegdfuyg')
+}
+alert(serviceName+" :"+serviceNumber)
+document.getElementById('coin-num').innerText=perClickCoin;
+
+ console.log(perClickCoin)
 
 
 
 
-    
  }
 })
